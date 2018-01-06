77f1c62eb73e14b6007c201cc0f175d3e398062d
Francis Redido <sicnarfodider@hotmail.com>
2018-01-06 15:43:53 -0800

    common theme finished, will have to polish soon per suggstion

--- src/components/app.js
+++ src/components/app.js
@@ -9,19 +9,30 @@ import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import reactbg from '../assets/images/react.svg';
 
-const App = () => (
-    <div id="main">
-        <div className="container">
-              <img className="main-bg-logo" src={reactbg} />
-              <Header />
-              <div id="main-content" className="row justify-content-md-center">
-                  <div className="nav-container col-lg-3">
-                      <Nav projects={projects} prototypes={prototypes}/>
-                  </div>
-              <Route path="/:type?/:content?" component={Content}/>
-              </div>
-        </div>
-    </div>
-);
 
-export default App;
+export default class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state={
+      fullContent: false
+    }
+  }
+
+  render(){
+    return(
+      <div id="main">
+          <div className="container">
+                <img className="main-bg-logo" src={reactbg} />
+                <Header />
+                <div id="main-content" className="row justify-content-md-center">
+                    <div className="nav-container col-lg-3">
+                        <Nav projects={projects} prototypes={prototypes}/>
+                    </div>
+                <Route path="/:type?/:content?" stuff="stuff" component={Content}/>
+                </div>
+          </div>
+      </div>
+    )
+  }
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import lfz from '../assets/images/lfz.png';
+import '../assets/css/header.css';
+import lfz from '../assets/images/lfzlogo.png';
 
 
 
 export default props=>{
   return(
 
-      <header className="text-center">
+      <header className="mt-3 text-center">
         <h1> React Prototypes </h1>
-        <img className="lfz" src={lfz} />
+        <div className="lfz"><img src={lfz} /><span className="lfz mr-3">LearningFuze</span></div>
       </header>
 
   )
